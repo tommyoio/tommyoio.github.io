8f4b2f4f4af27b843a764ca3f73a41ecaff98921
tommix <97884088+tommyoio@users.noreply.github.com>
2022-02-12 23:47:14 +0100

    Add files via upload

---
+++ Tarot/script.js
@@ -0,0 +1,12 @@
+function displayKey(keyNumber) {
+    switch (keyNumber) {
+        case 0:
+            alert('The Fool... blabla');
+            break;
+        case 1:
+            alert('The Magician... blablabla');
+            break;
+        case 2:
+            alert('The High Priestess... blablabla')
+    }
+}
